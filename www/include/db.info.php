64e94f794f45e28b18d201e3324d5db50499cbc5
Sebin Benjamin <sebinbenjamin@gmail.com>
2019-10-19 11:14:54 +0530

    feat: add dockerfile, docker-compose setup

--- www/include/db.info.php
+++ www/include/db.info.php
@@ -1,6 +1,6 @@
 <?php
   
-  define('DbInfo','mysql:host=localhost;dbname=wcms');
+  define('DbInfo','mysql:host=mysql;dbname=wcms');
   define('DbUser','wcms-root');
   define('DbPwd','wcms-root-password');
 
